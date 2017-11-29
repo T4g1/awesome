8e8e9c565fcf48ef4988b65fce64442684e839cd
Thomas Van Gysegem <thomasvangysegem@gmail.com>
2017-11-29 10:47:55 +0100

    [add] Initial commit

---
+++ autorun.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+function run {
+    count=1;
+
+    if [ ! -z $2 ] ;
+    then
+        count=$2
+    fi
+
+    result=$(pgrep -c $1);
+    echo $result;
+    while [ "$result" -lt "$count" ] ;
+    do
+        $1&
+        result=$(pgrep -c $1);
+    done
+}
+
+run chromium
+run subl3
+run xterm 4
